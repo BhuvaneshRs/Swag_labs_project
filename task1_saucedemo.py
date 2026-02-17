cdb44c8ef760520c18327dafa9338d230c5932aa
Bhuvaneshwara Ragavendra Srinivasan <132887722+BhuvaneshRs@users.noreply.github.com>
2026-02-17 13:23:27 +0530

    task1_saucedemo.py

--- task1_saucedemo.py
+++ task1_saucedemo.py
@@ -4,34 +4,28 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.edge.service import Service
 import time
 
-# Setup Edge
 service = Service("msedgedriver.exe")
 driver = webdriver.Edge(service=service)
 
 driver.maximize_window()
 driver.get("https://www.saucedemo.com/")
 
-# Login
 driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
-
 time.sleep(2)
 
-# Sort Options list
 sort_options = ["lohi", "hilo", "az"]
 
 for option in sort_options:
 
     print(f"\nTesting sort option: {option}")
 
-    # Select sorting
     dropdown = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
     dropdown.select_by_value(option)
 
     time.sleep(2)
 
-    # Get all products
     products = driver.find_elements(By.CLASS_NAME, "inventory_item")
 
     first_product = products[0]
@@ -42,10 +36,8 @@ for option in sort_options:
     print("Selected Product:", product_name)
     print("Price:", product_price)
 
-    # Add to cart
     first_product.find_element(By.TAG_NAME, "button").click()
 
-    # Go to cart
     driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
     time.sleep(2)
 
@@ -53,15 +45,12 @@ for option in sort_options:
     cart_price = driver.find_element(By.CLASS_NAME, "inventory_item_price").text
     cart_quantity = driver.find_element(By.CLASS_NAME, "cart_quantity").text
 
-    # Verification
     assert cart_name == product_name
     assert cart_price == product_price
     assert cart_quantity == "1"
 
-    assert cart_price == product_price
+    print("Verification Success")
 
-
-    # Remove item & go back
     driver.find_element(By.CLASS_NAME, "cart_button").click()
     driver.find_element(By.ID, "continue-shopping").click()
 
